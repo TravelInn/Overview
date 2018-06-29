5e743734b0c8290a88486714b3dd04ae48aaa647
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-06-29 15:11:33 -0700

    Added two addition get endpoints to server

--- server/server.js
+++ server/server.js
@@ -55,6 +55,65 @@ app.get('/api/overview/:id', async (req, res) => {
   }
 });
 
+app.get('/api/hostels', async (req, res) => {
+  try {
+
+    const query = `SELECT city, country FROM location WHERE ID > $1 LIMIT 20;`;
+
+    db.any({
+      name: 'return-set-of-locations',
+      text: query,
+      values: Math.floor(Math.random() * 999980) + 1,
+    })
+      .then(data => res.send(data));
+
+  } catch (error) {
+    res.status(404).send(`ERROR: ${error}`);
+  }
+});
+
+app.get('/api/hostels/:id/info', async (req, res) => {
+  try {
+
+    // for now, just returning a random hostel
+    // in the future, should refactor to be a random selection from hostels with locationid of input
+
+    const query = `
+    SELECT htable.id, name, description, avg_rating, urls.array
+    FROM hostel AS htable
+    INNER JOIN 
+      (SELECT hostelid, AVG(rating) AS avg_rating
+      FROM review
+      WHERE hostelid=$1
+      GROUP BY hostelid)
+      AS rtable 
+      ON htable.id = rtable.hostelid
+      INNER JOIN
+        (SELECT $1 AS id, 
+        ARRAY
+          (SELECT url
+          FROM 
+            (SELECT UNNEST(photosarrayids)
+            FROM hostel
+            WHERE id = $1)
+            AS photoidtable
+            INNER JOIN
+              photos
+              ON photoidtable.unnest = photos.id)
+      ) 
+    AS urls ON urls.id = htable.id`;
+
+    db.one({
+      name: 'return-hostel-info',
+      text: query,
+      values: Math.floor(Math.random() * 10000000) + 1,
+    })
+      .then(data => res.send(data));
+  } catch (error) {
+    res.status(404).send(`ERROR: ${error}`);
+  }
+});
+
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
